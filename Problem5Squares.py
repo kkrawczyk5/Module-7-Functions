6487043b3a4f1977fa34a86ebdfbab96513adecb
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-03-04 12:38:34 -0600

    Update Problem5Squares.py

--- Problem5Squares.py
+++ Problem5Squares.py
@@ -29,3 +29,29 @@ for n in range(10, 60, 10):
   new_position = new_position -5
 
 wn.exitonclick()
+
+#import turtle
+#def drawSquare(t, sz):
+#   """Get turtle t to draw a square of sz side"""
+#   for i in range(4):
+#       t.forward(sz)
+#        t.left(90)
+        
+#wn = turtle.Screen()
+
+#alex = turtle.Turtle()
+#alex.color("blue")
+
+#size = [20, 40, 60, 80, 100]
+
+#for x in size:
+#    drawSquare(alex,x)
+#    alex.penup()
+#    alex.backward(10)       # move alex to the starting position for the next square
+#    alex.right(90)
+#    alex.forward(10)
+#    alex.left(90)
+#    alex.pendown()
+
+#wn.exitonclick()
+
